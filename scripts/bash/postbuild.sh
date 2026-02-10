d4859215bd58b9f5d200f14622ac46bd25e2df98
AntGH <37946390+AntGH@users.noreply.github.com>
2026-02-09 19:17:36 -0500

    add message on complete to to latest log-*.txt

--- scripts/bash/postbuild.sh
+++ scripts/bash/postbuild.sh
@@ -18,11 +18,17 @@ curl -sS https://$WEBSITE_HOSTNAME/install.php?auto=1
 
 echo -e "\nFinished running install.php"
 
+# Append to the same deploy log used by deploy.sh ($stamp not available in PostDeploymentActions)
+LOG=$(ls -t /home/site/log-*.txt 2>/dev/null | head -1)
+if [ -n "$LOG" ]; then
+  echo "PostDeploymentActions: postbuild.sh executed (install.php?auto=1 completed)." >> "$LOG"
+fi
+
 ####################################################################################
 #
 # Update additional configuration settings including
 # user file uploading settings to Azure Blob Storage
-# 
+#
 ####################################################################################
 
 #bash /home/site/repository/scripts/bash/install.sh
